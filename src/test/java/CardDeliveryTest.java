2501e0d6a24ab7b00887d894da9a760487888191
Neto test <2001107@mail.ru>
2024-05-01 15:02:10 +0300

    DZ modification

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.proxy.SelenideProxyServer;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -8,8 +9,7 @@ import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
     private String generateData(int addDays, String pattern) {
@@ -35,8 +35,8 @@ public class CardDeliveryTest {
     @Test
     void testForm() {
         open("http://localhost:9999/");
-        $("[data-test-id = 'city'] input").setValue("Ря");
-        $(byText("Рязань")).click();
+        $("[data-test-id = 'city'] input").setValue("Во");
+        $$(".menu-item__control").find(exactText("Воронеж")).click();
 
         String planningDate = generateData(3, "dd.MM.yyyy");
         $("[data-test-id = 'date'] input").doubleClick().sendKeys(Keys.DELETE);
@@ -53,19 +53,24 @@ public class CardDeliveryTest {
     @Test
     void testCalendar() {
         open("http://localhost:9999/");
-        $("[data-test-id = 'city'] input").setValue("Ря");
-        $(byText("Рязань")).click();
+        $("[data-test-id = 'city'] input").setValue("Во");
+        $$(".menu-item__control").find(exactText("Воронеж")).click();
 
+        String planningDate = generateData(3, "dd.MM.yyyy");
+        $("[data-test-id = 'date'] input").click();
+        if (!generateData(3, "MM").equals(generateData(7, "MM"))){
+            $("[data-step='1'] input").click();
+        }
+        $$(".calendar__day").findBy(Condition.text(generateData(7,"d"))).click();
 
-        $("button.icon-button").click();
-        $(byText("5")).click();
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id = 'name'] input").setValue("Зеленский Александр");
         $("[data-test-id = 'phone'] input").setValue("+79040994050");
         $("[data-test-id = 'agreement']").click();
         $("button.button").click();
         $("[data-test-id='notification']")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(exactText("Успешно! Встреча успешно забронирована на 05.05.2024"));
+                .shouldHave(exactText("Успешно! Встреча успешно забронирована на 08.05.2024"));
 
 
     }
